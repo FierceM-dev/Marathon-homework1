4426d2938206bd641b4ca574a4d1338ac7ee2650
FierceM-dev <79756698+FierceM-dev@users.noreply.github.com>
2022-04-17 14:44:02 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function () {
+	$(window).scroll(function() {
+	    $('#learn .section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.input').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("zoomIn");
+	        }
+	    });
+	});
+})
